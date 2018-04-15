8d70acf3d55792995bd82512cf8ea20a8435f1fa
kolmio23 <kolmio23@users.noreply.github.com>
2018-04-15 21:21:05 +0200

    add country data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
-import Question from "./Question";
+import getCountries from './api';
+import EnterName from './EnterName';
 
 class App extends Component {
   constructor(props) {
@@ -9,6 +10,10 @@ class App extends Component {
     this.handlerUsername = this.handlerUsername.bind(this);
     this.handlerTraining = this.handlerTraining.bind(this);
     this.handlerStats = this.handlerStats.bind(this);
+
+    let c = getCountries();
+    console.log(c)
+
   }
 
   handlerUsername(username) {
@@ -37,67 +42,6 @@ class App extends Component {
         <Status username={this.state.username} training={this.state.training} correct={this.state.correct} wrong={this.state.wrong}/>
       </div>
     );
-  }
-}
-
-
-
-class EnterName extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {username: ""};
-    this.handleChange = this.handleChange.bind(this);
-    this.handleKeyPress = this.handleKeyPress.bind(this);
-    this.chooseName = this.chooseName.bind(this);
-    this.chooseTraining = this.chooseTraining.bind(this);
-  }
-
-  render() {
-    if( this.props.what === "select") {
-
-      return (
-        <div>
-          <p>Was möchtest du üben?</p>
-          <p/>
-          <button onClick={this.chooseTraining}>Länder Europas</button><p/>
-          <button onClick={this.chooseTraining}>Multiplikation</button><p/>
-          <button onClick={this.chooseTraining}>Division</button><p/>
-        </div>
-      );
-
-    } else if( this.props.what === "train") {
-
-      return <Question handlerStats={this.props.handlerStats}/>
-
-    } else {
-
-      return (
-        <div>
-          <p>Wie heißt du?</p>
-          <input id="name" type="text" size="40" value={this.state.username} onKeyPress={this.handleKeyPress} onChange={this.handleChange}/>
-          <p/>
-          <button onClick={this.chooseName}>Los gehts</button>
-        </div>
-      );
-    }
-  }
-
-  handleChange(event) {
-    this.setState({username: event.target.value});
-  }
-
-  handleKeyPress(event) {
-    if (event.key === 'Enter') {
-      this.chooseName()
-    }
-  }
-
-  chooseName() {
-    this.props.handlerUsername(this.state.username)
-  }
-
-  chooseTraining() {
-    this.props.handlerTraining("Länder Europas")
   }
 }
 
---
+++ src/EnterName.js
@@ -0,0 +1,65 @@
+import React, {Component} from 'react';
+import Question from './Question';
+
+export default class EnterName extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {username: ""};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
+        this.chooseName = this.chooseName.bind(this);
+        this.chooseTraining = this.chooseTraining.bind(this);
+    }
+
+    render() {
+        if (this.props.what === "select") {
+
+            return (
+                <div>
+                    <p>Was möchtest du üben?</p>
+                    <p/>
+                    <button onClick={this.chooseTraining}>Länder Europas</button>
+                    <p/>
+                    <button onClick={this.chooseTraining}>Multiplikation</button>
+                    <p/>
+                    <button onClick={this.chooseTraining}>Division</button>
+                    <p/>
+                </div>
+            );
+
+        } else if (this.props.what === "train") {
+
+            return <Question handlerStats={this.props.handlerStats}/>
+
+        } else {
+
+            return (
+                <div>
+                    <p>Wie heißt du?</p>
+                    <input id="name" type="text" size="40" value={this.state.username} onKeyPress={this.handleKeyPress}
+                           onChange={this.handleChange}/>
+                    <p/>
+                    <button onClick={this.chooseName}>Los gehts</button>
+                </div>
+            );
+        }
+    }
+
+    handleChange(event) {
+        this.setState({username: event.target.value});
+    }
+
+    handleKeyPress(event) {
+        if (event.key === 'Enter') {
+            this.chooseName()
+        }
+    }
+
+    chooseName() {
+        this.props.handlerUsername(this.state.username)
+    }
+
+    chooseTraining() {
+        this.props.handlerTraining("Länder Europas")
+    }
+}
